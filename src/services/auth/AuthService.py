b50d8d6121a7f3ef2bc567053d4e360b1114681b
BorwinTheJoe <mateusz.jaltoszuk@gmail.com>
2026-04-25 13:05:37 +0200

    added register method

--- src/services/auth/AuthService.py
+++ src/services/auth/AuthService.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
-from src.models import UserDTO
+from src.models import UserDTO, RegisterDTO
 
 class AuthService(BaseModel):
     @staticmethod
-    def login(userDTO: UserDTO):
+    def login(userDTO: UserDTO) -> str:
         if len(userDTO.username) != 0:
             output_str = "Nazwa Użytkownika: " + userDTO.username + "\n"
             print("Nazwa Użytkownika: " + userDTO.username + "\n")
@@ -17,5 +17,15 @@ class AuthService(BaseModel):
         else:
             output_str += "Brak hasła użytkownika"
             print("Brak hasła użytkownika")
+
+        return output_str
+    
+    @staticmethod
+    def register(registerDTO: RegisterDTO) -> str:
+        if len(registerDTO.username) != 0:
+            output_str = "Nazwa Użytkownika: " + registerDTO.username + "\n"
+            print ("nazwa Użytkownika: " + registerDTO.username + "\n")
+        else:
+            output_str = "Brak nazwy użytkownika\n"
 
         return output_str
